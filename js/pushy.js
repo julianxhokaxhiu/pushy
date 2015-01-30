fa54c5f50cd80ebe4028d431932ee125da059f86
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2015-01-30 13:00:28 +0100

    A better way to write javascript + GCC minified version.

--- js/pushy.js
+++ js/pushy.js
@@ -5,27 +5,30 @@
 
 (function($) {
 	$(function(){
-		var pushy = $('.pushy'), //menu css class
-			body = $('body'),
-			container = $('.pushy-container'), //container css class
-			siteOverlay = $('.pushy-site-overlay'), //site overlay
-			pushyActiveClass = "pushy-active", //css class to toggle site overlay
-			menuBtn = $('.pushy-menu-btn'), //css classes to toggle the menu
+		var $body = $('body'),
+			$pushy = $('.pushy'), //menu css class
+			$container = $('.pushy-container'), //container css class
+			$siteOverlay = $('.pushy-site-overlay'), //site overlay
+			$menuBtn = $('.pushy-menu-btn'), //css classes to toggle the menu
+			pushyActiveClass = 'pushy-active', //css class to toggle site overlay
 			onClickHandler = function (e) {
 				e.preventDefault();
-				body.toggleClass(pushyActiveClass); //toggle site overlay
+				$body.toggleClass(pushyActiveClass); //toggle site overlay
 			}
 
-		pushy.on('click', 'a.closePushy', onClickHandler);
+		// Close pushy if a specified item has 'closePushy' class
+		$pushy
+		.on('click', 'a.closePushy', onClickHandler);
 
 		//toggle menu
-		menuBtn.on('click', onClickHandler);
+		$menuBtn
+		.on('click', onClickHandler);
 
 		//close menu when clicking site overlay
-		siteOverlay.on('click', onClickHandler);
+		$siteOverlay
+		.on('click', onClickHandler);
 
-		$(function(e){
-			pushy.removeClass('pushy-static');
-		})
+		$pushy
+		.removeClass('pushy-static');
 	});
 })(jQuery);
